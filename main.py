59241851397729e453e0a1c5b827fffe6d6896f5
SANOGOTECH <sanogotech@yahoo.fr>
2020-09-25 11:11:29 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import time
+
+filePath = '/var/log/logfile.log'
+
+lastLine = None
+with open(filePath,'r') as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            print(line)
+            lastLine = line
+
+while True:
+    with open(filePath,'r') as f:
+        lines = f.readlines()
+    if lines[-1] != lastLine:
+        lastLine = lines[-1]
+        print(lines[-1])
+    time.sleep(1)
